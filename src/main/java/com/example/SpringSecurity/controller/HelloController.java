7be21a544ac5be37f4eae60ce712849cb3d00c16
Manish0085 <manishk57107@gmail.com>
2025-08-13 18:06:57 +0530

    How to create own user and password

---
+++ src/main/java/com/example/SpringSecurity/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.SpringSecurity.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String greet(HttpServletRequest request){
+
+        return "Session Id: "+request.getSession().getId();
+    }
+}
