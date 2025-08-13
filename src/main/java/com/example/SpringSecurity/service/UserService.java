3545e0c7a8c16b88855adefe82d937a482598b7f
Manish0085 <manishk57107@gmail.com>
2025-08-13 23:53:33 +0530

    Token Generation

--- src/main/java/com/example/SpringSecurity/service/UserService.java
+++ src/main/java/com/example/SpringSecurity/service/UserService.java
@@ -4,6 +4,9 @@ package com.example.SpringSecurity.service;
 import com.example.SpringSecurity.entity.Users;
 import com.example.SpringSecurity.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,11 +17,29 @@ public class UserService {
     @Autowired
     private UserRepo userRepo;
 
+    @Autowired
+    private JWTService jwtService;
+
+    @Autowired
+    private AuthenticationManager authManager;
+
     private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
     public Users register(Users user){
         user.setPassword(encoder.encode(user.getPassword()));
         return userRepo.save(user);
 
+    }
+
+    public String verify(Users user) {
+        Authentication authentication =
+                authManager.authenticate(new UsernamePasswordAuthenticationToken(
+                        user.getUsername(),
+                        user.getPassword()
+                ));
+        if (authentication.isAuthenticated())
+            return jwtService.generateToken(user.getUsername());
+
+        return "fail";
     }
 }
