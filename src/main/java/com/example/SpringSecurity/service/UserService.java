152f67eaeafde7ab09d33184ec6dd1aa2f2bf1ce
Manish0085 <manishk57107@gmail.com>
2025-08-13 22:58:28 +0530

    Basic auth eith password encryption

---
+++ src/main/java/com/example/SpringSecurity/service/UserService.java
@@ -0,0 +1,24 @@
+package com.example.SpringSecurity.service;
+
+
+import com.example.SpringSecurity.entity.Users;
+import com.example.SpringSecurity.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+
+    @Autowired
+    private UserRepo userRepo;
+
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
+    public Users register(Users user){
+        user.setPassword(encoder.encode(user.getPassword()));
+        return userRepo.save(user);
+
+    }
+}
